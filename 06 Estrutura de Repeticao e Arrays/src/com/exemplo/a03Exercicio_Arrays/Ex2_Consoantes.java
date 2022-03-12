9204359aab70d080cf793af0d177c67c690c97a4
Mauricio <96099724+mauriciolb87@users.noreply.github.com>
2022-03-11 23:29:43 -0300

    ....

--- 06 Estrutura de Repeticao e Arrays/src/com/exemplo/a03Exercicio_Arrays/Ex2_Consoantes.java
+++ 06 Estrutura de Repeticao e Arrays/src/com/exemplo/a03Exercicio_Arrays/Ex2_Consoantes.java
@@ -7,5 +7,50 @@ Imprima as consoantes.
 */
 
 
+import java.util.Scanner;
+
 public class Ex2_Consoantes {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] caracterCon = new String[6];
+        String[] caracterVol = new String[6];
+        int qtdConsoantes = 0;
+        int qtdVogais = 0;
+        int count = 0;
+
+        do {
+            System.out.print("Letra: " );
+            String letra = scan.next();
+
+            if (letra.equalsIgnoreCase("a") |
+                letra.equalsIgnoreCase("e") |
+                letra.equalsIgnoreCase("i") |
+                letra.equalsIgnoreCase("o") |
+                letra.equalsIgnoreCase("u") )
+            {
+                caracterVol[count] = letra;
+                qtdVogais++;
+            }else {
+                caracterCon[count] = letra;
+                qtdConsoantes++;
+            }
+
+            count++;
+        }while ( count < caracterCon.length);
+
+        for (String caracters : caracterCon  ) {
+            if (caracters != null) {
+                System.out.println("Consoantes:" + caracters);
+            }
+        }
+        for (String caracters : caracterVol  ) {
+            if (caracters != null) {
+                System.out.println("Vogais:" + caracters);
+            }
+        }
+
+    }
 }
--- 06 Estrutura de Repeticao e Arrays/src/com/exemplo/a03Exercicio_Arrays/Ex3_NumerosAleatorios.java
+++ 06 Estrutura de Repeticao e Arrays/src/com/exemplo/a03Exercicio_Arrays/Ex3_NumerosAleatorios.java
@@ -5,5 +5,59 @@ Faça um Programa que leia 20 números inteiros aleatórios (entre 0 e 100) arma
 Ao final mostre os números e seus sucessores.
 */
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex3_NumerosAleatorios {
+
+    public static void main(String[] args) {
+
+        exer01();
+        //exer02();
+    }
+
+    private static void exer01() {
+
+        Integer[] numero = new Integer[20];
+        Random gerador = new Random();
+        int count = 0;
+
+        do {
+            int num = gerador.nextInt(101);
+
+            numero[count] = num;
+            count++;
+
+        }while ( count < numero.length);
+
+        for (int num : numero  ) {
+
+             System.out.println("Numero:" + num + " Antecessor:" + (num - 1) + " Sucessores : " + (num + 1)  );
+        }
+    }
+
+    public static void exer02(){
+
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for (int i = 0; i < numerosAleatorios.length; i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Numeros Aleatorios");
+        for ( int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+        System.out.println("\nSucessores dos Numeros Aleatorios");
+        for ( int numero : numerosAleatorios) {
+            System.out.println((numero +1) + " ");
+        }
+        System.out.println("\nAntecessor dos Numeros Aleatorios");
+        for ( int numero : numerosAleatorios) {
+            System.out.println((numero -1) + " ");
+        }
+    }
 }
